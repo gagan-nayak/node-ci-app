27a20f3aebec0d6d13df65745d97fbf32c594e97
gagan.nayak <gagan.nayak@sharaaninfo.com>
2025-04-10 17:27:11 +0530

    Added two more test cases in app.test.js

--- app.test.js
+++ app.test.js
@@ -3,3 +3,11 @@ const add = require('./app');
 test('adds 2 + 3 + 5 to equal 10', () => {
   expect(add(2, 3, 5)).toBe(10);
 });
+
+test('adds 8 + 3 + 4 to equal 15', () => {
+  expect(add(8, 3, 4)).toBe(15);
+});
+
+test('adds 2 + 6 + 5 to equal 13', () => {
+  expect(add(2, 6, 5)).toBe(13);
+});
